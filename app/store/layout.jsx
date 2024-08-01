8171297eeb4bbd87e3af9b6ecf59f493cc0f88f5
Russ Madrigal <71038002+rusbenmadrigal@users.noreply.github.com>
2024-07-31 19:54:33 -0600

    Link

--- app/store/layout.jsx
+++ app/store/layout.jsx
@@ -1,13 +1,23 @@
+import Link from "next/link";
 export default function StoreLayout({ children }) {
-  return;
-  <>
-    <nav>
-      <h3>Store Layout</h3>
-      <ul>
-        <li>Categories</li>
-        <li>Products</li>
-      </ul>
-    </nav>
-    {children}
-  </>;
+  return (
+    <>
+      <nav>
+        <h3>Store Layout</h3>
+
+        <ul>
+          <li>
+            <Link href="/store/">Store</Link>
+          </li>
+          <li>
+            <Link href="/store/categories/">Categories</Link>
+          </li>
+          <li>
+            <Link href="/store/categories/laptop/">Laptop</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
 }
